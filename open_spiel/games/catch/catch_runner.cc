21c5e38628ad340ef17406c4c1c747158350b794
Massimo Fioravanti <massimo.fioravanti@polimi.it>
2025-04-21 13:13:56 +0200

    more benchmarks

---
+++ open_spiel/games/catch/catch_runner.cc
@@ -0,0 +1,8 @@
+
+
+#include "open_spiel/games/catch/catch.h"
+// 
+const char* name = "catch_open_spiel.h";
+using GameType = open_spiel::catch_::CatchGame;
+using StateType = open_spiel::catch_::CatchState;
+#include "../benchmark_utils.h"
---
+++ open_spiel/games/connect_four/connect_four_runner.cc
@@ -0,0 +1,8 @@
+
+
+#include "open_spiel/games/connect_four/connect_four.h"
+// 
+const char* name = "connect_four_open_spiel.h";
+using GameType = open_spiel::connect_four::ConnectFourGame;
+using StateType = open_spiel::connect_four::ConnectFourState;
+#include "../benchmark_utils.h"
